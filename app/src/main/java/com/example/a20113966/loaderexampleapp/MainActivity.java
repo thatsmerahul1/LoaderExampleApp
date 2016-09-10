10e44f75596065d65eb028d320f7d5584027775c
Rahul Maurya <thatsmerahul1@gmail.com>
2016-09-10 12:15:41 +0530

    New Update changes

--- app/src/main/java/com/example/a20113966/loaderexampleapp/MainActivity.java
+++ app/src/main/java/com/example/a20113966/loaderexampleapp/MainActivity.java
@@ -26,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b1=(Button)findViewById(R.id.button);
 
+
+
+
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
